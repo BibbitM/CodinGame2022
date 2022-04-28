620b1b9efc5adbcb41870b1ef0d359f844cc33b8
Michał Nowak <bibbit@wp.pl>
2022-04-29 01:31:27 +0200

    Creating 3 paladins

--- SpringChallenge.cpp
+++ SpringChallenge.cpp
@@ -465,9 +465,9 @@ void Game::MakeMove(std::ostream& out) const
 void Game::PossesEntity(Entity* hero)
 {
 	std::unique_ptr<Controller> controller{};
-	if (myHeroes.empty())
-		controller = std::make_unique<PeasantController>(*hero);
-	else
+	//if (myHeroes.empty())
+	//	controller = std::make_unique<PeasantController>(*hero);
+	//else
 		controller = std::make_unique<PaladinController>(*hero);
 
 	hero->SetController(std::move(controller));
