bd18ac87aa34683d244af2a924c46625c8bb9090
Michał Nowak <bibbit@wp.pl>
2022-04-29 14:48:16 +0200

    Added FunctionTimer to measure function execution time

---
+++ Codin/Timer.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "Utils.h"
+
+#include <chrono>
+#include <iostream>
+#include <string_view>
+
+#define MEASURE_EXECUTION_TIME 1
+
+// https://stackoverflow.com/questions/22387586/measuring-execution-time-of-a-function-in-c
+
+class FunctionTimer
+{
+public:
+	FunctionTimer(std::string_view fuctionName)
+		: start(std::chrono::high_resolution_clock::now())
+		, functionName(fuctionName)
+	{}
+
+	~FunctionTimer()
+	{
+		std::chrono::high_resolution_clock::time_point end = std::chrono::high_resolution_clock::now();
+		std::cerr << functionName << ' ' << std::chrono::duration<float, std::milli>(end - start).count() << "ms" << std::endl;
+	}
+
+private:
+	std::chrono::high_resolution_clock::time_point start;
+	std::string_view functionName;
+};
+
+#if MEASURE_EXECUTION_TIME
+#define FUNCTION_TIMER() FunctionTimer UNIQUE_NAME(FunctionTimer){ __FUNCTION__ }
+#else
+#define FUNCTION_TIMER() (void)(0)
+#endif
--- Codin/Utils.h
+++ Codin/Utils.h
@@ -1,3 +1,14 @@
 #pragma once
 
+/// Mark that given parameter is not used at all in the function.
 #define UNUSED(x) (void)(x)
+/// Mark that given parameter is not used in given code path.
+#define TOUCH(x) (void)(x)
+
+// https://stackoverflow.com/questions/1082192/how-to-generate-random-variable-names-in-c-using-macros
+// One level of macro indirection is required in order to resolve __COUNTER__,
+// and get varname1 instead of varname__COUNTER__.
+#define CONCAT(a, b) CONCAT_INNER(a, b)
+#define CONCAT_INNER(a, b) a ## b
+
+#define UNIQUE_NAME(base) CONCAT(base, __COUNTER__)
